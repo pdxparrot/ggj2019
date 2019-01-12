8899f7bab405a89a7d14b62700da182450444256
Shane Lillie <ignignokterr@gmail.com>
2019-01-12 11:38:44 -0800

    Add server spectator to the Game code

---
+++ Assets/Scripts/Game/Camera/ServerSpectatorViewer.cs
@@ -0,0 +1,18 @@
+﻿using pdxpartyparrot.Core.Camera;
+using pdxpartyparrot.Game.Actors;
+
+namespace pdxpartyparrot.Game.Camera
+{
+    public sealed class ServerSpectatorViewer : FollowViewer
+    {
+        public void Initialize(ServerSpectator owner)
+        {
+            Initialize(0);
+
+            Set3D();
+
+            FollowCamera.SetTarget(owner.FollowTarget);
+            SetFocus(owner.transform);
+        }
+    }
+}
