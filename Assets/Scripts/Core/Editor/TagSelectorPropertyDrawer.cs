5c30c91785b892c360c8f31b97027e0ff23cf453
Shane Lillie <ignignokterr@gmail.com>
2019-03-08 21:06:13 -0800

    Reorderable lists

--- Assets/Scripts/Core/Editor/TagSelectorPropertyDrawer.cs
+++ Assets/Scripts/Core/Editor/TagSelectorPropertyDrawer.cs
@@ -5,61 +5,64 @@ using UnityEditor;
 
 // http://www.brechtos.com/tagselectorattribute/
 
-//Original by DYLAN ENGELMAN http://jupiterlighthousestudio.com/custom-inspectors-unity/
-//Altered by Brecht Lecluyse http://www.brechtos.com
+// original by DYLAN ENGELMAN http://jupiterlighthousestudio.com/custom-inspectors-unity/
+// altered by Brecht Lecluyse http://www.brechtos.com
 
 namespace pdxpartyparrot.Core.Util.Editor
 {
-    [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+    [CustomPropertyDrawer(typeof(TagSelectorAttribute), true)]
     public class TagSelectorPropertyDrawer : PropertyDrawer
     {
 #region Unity Lifecycle
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if(property.propertyType == SerializedPropertyType.String) {
-                EditorGUI.BeginProperty(position, label, property);
- 
+            if(SerializedPropertyType.String != property.propertyType) {
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
+
+            EditorGUI.BeginProperty(position, label, property);
+            try {
                 TagSelectorAttribute attrib = (TagSelectorAttribute)attribute;
                 if(attrib.UseDefaultTagFieldDrawer) {
                     property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+                    return;
+                }
+
+                // generate the taglist + custom tags
+                var tagList = new List<string> { "<NoTag>" };
+                tagList.AddRange(UnityEditorInternal.InternalEditorUtility.tags);
+
+                string propertyString = property.stringValue;
+
+                int index = -1;
+                if(propertyString =="") {
+                    // the tag is empty
+                    index = 0; // first index is the special <notag> entry
                 } else {
-                    //generate the taglist + custom tags
-                    List<string> tagList = new List<string> { "<NoTag>" };
-                    tagList.AddRange(UnityEditorInternal.InternalEditorUtility.tags);
-
-                    string propertyString = property.stringValue;
-
-                    int index = -1;
-                    if(propertyString =="") {
-                        //The tag is empty
-                        index = 0; //first index is the special <notag> entry
-                    } else {
-                        //check if there is an entry that matches the entry and get the index
-                        //we skip index 0 as that is a special custom case
-                        for(int i=1; i<tagList.Count; ++i) {
-                            if(tagList[i] == propertyString) {
-                                index = i;
-                                break;
-                            }
+                    // check if there is an entry that matches the entry and get the index
+                    // we skip index 0 as that is a special custom case
+                    for(int i=1; i<tagList.Count; ++i) {
+                        if(tagList[i] == propertyString) {
+                            index = i;
+                            break;
                         }
                     }
-                
-                    //Draw the popup box with the current selected index
-                    index = EditorGUI.Popup(position, label.text, index, tagList.ToArray());
- 
-                    //Adjust the actual string value of the property based on the selection
-                    if(index == 0) {
-                        property.stringValue = "";
-                    } else if(index >= 1) {
-                        property.stringValue = tagList[index];
-                    } else {
-                        property.stringValue = "";
-                    }
                 }
- 
+            
+                // draw the popup box with the current selected index
+                index = EditorGUI.Popup(position, label.text, index, tagList.ToArray());
+
+                // adjust the actual string value of the property based on the selection
+                if(index == 0) {
+                    property.stringValue = "";
+                } else if(index >= 1) {
+                    property.stringValue = tagList[index];
+                } else {
+                    property.stringValue = "";
+                }
+            } finally {
                 EditorGUI.EndProperty();
-            } else {
-                EditorGUI.PropertyField(position, property, label);
             }
         }
 #endregion
---
+++ Assets/Scripts/Core/Util/ReorderableList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace pdxpartyparrot.Core.Util
+{
+    [Serializable]
+    public class ReorderableList<T>
+    {
+        [SerializeField]
+        private List<T> _items = new List<T>();
+
+        public List<T> Items => _items;
+    }
+
+    [Serializable]
+    public class ReorderableListString : ReorderableList<string>
+    {
+    }
+
+    [Serializable]
+    public class ReorderableListInt : ReorderableList<int>
+    {
+    }
+
+    [Serializable]
+    public class ReorderableListFloat : ReorderableList<float>
+    {
+    }
+
+    [Serializable]
+    public class ReorderableListGameObject : ReorderableList<GameObject>
+    {
+    }
+}
