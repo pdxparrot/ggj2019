f899a7f6266883ee3ceb4086072ccd98b26191eb
Shane Lillie <ignignokterr@gmail.com>
2019-04-13 14:29:25 -0700

    Remove player prefs reset

--- Assets/Scripts/Core/Editor/EditorUtils.cs
+++ Assets/Scripts/Core/Editor/EditorUtils.cs
@@ -5,13 +5,14 @@ namespace pdxpartyparrot.Core.Editor
 {
     public static class EditorUtils
     {
-        [MenuItem("PDX Party Parrot/Reset PlayerPrefs")]
+        // this functionality is now available in Unity's menus
+        /*[MenuItem("PDX Party Parrot/Reset PlayerPrefs")]
         static void ResetPlayerPrefs()
         {
             if(EditorUtility.DisplayDialog("Reset Player Prefs", "Are you sure you wish to reset PlayerPrefs?", "Yes", "No")) {
                 PlayerPrefs.DeleteAll();
                 EditorUtility.DisplayDialog("Reset Player Prefs", "PlayerPrefs reset!", "Ok");
             }
-        }
+        }*/
     }
 }
