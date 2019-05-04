d0a3594dcff12e0919ffb231ed7d51bbf51324a2
Shane Lillie <ignignokterr@gmail.com>
2019-05-04 09:34:18 -0700

    More stuff

--- Assets/Scripts/Core/Input/PauseActionHandler.cs
+++ Assets/Scripts/Core/Input/PauseActionHandler.cs
@@ -2,6 +2,7 @@
 
 namespace pdxpartyparrot.Core.Input
 {
+    // TODO: I don't think this is used in any way
     public interface IPauseActionHandler
     {
         void OnPause(InputAction.CallbackContext context);
